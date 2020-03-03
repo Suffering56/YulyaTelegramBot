462300feeb194066515fba72ffa987233d98ad05
v.peschanyi <p.v.s.oren@gmail.com>
2020-03-04 00:25:02 +0300

    - working

--- src/main/java/pvs/tgbot/example/App.java
+++ src/main/java/pvs/tgbot/example/App.java
@@ -4,15 +4,28 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
 /**
  * @author v.peschaniy
- *      Date: 27.02.2020
+ * Date: 27.02.2020
  */
 public class App {
 
-    public static void main(String[] args) throws TelegramApiRequestException {
+    public static void main(String[] args) throws IOException, TelegramApiRequestException {
+
+        System.out.println("YulyaTelegramBot started 3");
+
+        Properties textProperties = new Properties();
+        textProperties.load(new InputStreamReader(App.class.getResource("/text.properties").openStream(), Charset.forName("cp1251")));
+
+        System.out.println("textProperties.getProperty(\"start\") = " + textProperties.getProperty("start"));
+
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
-        api.registerBot(new Bot());
+        api.registerBot(new YulyaTelegramBot(textProperties));
     }
 }
---
+++ src/main/java/pvs/tgbot/example/YulyaTelegramBot.java
@@ -0,0 +1,141 @@
+package pvs.tgbot.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author v.peschaniy
+ * Date: 27.02.2020
+ */
+public class YulyaTelegramBot extends TelegramLongPollingBot {
+
+    private static final String BOT_USERNAME = "YulyaTelegramBot";
+    private static final String BOT_TOKEN = "999519992:AAGxKzcJJiX1UUsT8tsZcyhZ-A8Hn38UCcs";
+    private final Properties textProperties;
+
+    YulyaTelegramBot(Properties textProperties) {
+        this.textProperties = textProperties;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("onUpdateReceived");
+
+        if (update.hasInlineQuery()) {
+            System.out.println("INLINE QUERY");
+            System.out.println("u");
+
+        } else if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String callbackData = callbackQuery.getData();
+            System.out.println("callbackData = " + callbackData);
+
+            sendAnswerCallbackQuery(callbackQuery.getId(), callbackData);
+        } else {
+            Message inputMessage = update.getMessage();
+
+            String txt = inputMessage.getText();
+            System.out.println("inputMessage = " + txt);
+
+            if (txt.equals("/start")) {
+                sendAnswer(inputMessage.getChatId(), "start");
+            }
+        }
+    }
+
+    private void sendAnswerCallbackQuery(String callbackQueryId, String text) {
+        try {
+            AnswerCallbackQuery answer = new AnswerCallbackQuery()
+                    .setText(txt(text))
+                    .setCallbackQueryId(callbackQueryId);
+            sendApiMethod(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendAnswer(long chatId, String messageName) {
+        try {
+            sendApiMethod(makeAnswer(chatId, messageName));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SendMessage makeAnswer(long chatId, String messageName) {
+        SendMessage answer = new SendMessage();
+        answer.setChatId(chatId);
+
+        String text = txt(messageName);
+        answer.setText(text);
+
+        KeyboardRow keyboardRow = Stream.of("/run", "/cancel")
+                .map(KeyboardButton::new)
+                .collect(
+                        KeyboardRow::new,
+                        KeyboardRow::add,
+                        throwingMerge()
+                );
+
+        answer.setReplyMarkup(new ReplyKeyboardMarkup(
+                new ArrayList<KeyboardRow>() {{
+                    add(keyboardRow);
+                }}
+        ));
+
+        return answer;
+    }
+
+    private <T> BiConsumer<T, T> throwingMerge() {
+        return (t1, t2) -> {
+            throw new UnsupportedOperationException();
+        };
+    }
+
+    private String txt(String messageName) {
+        return textProperties.getProperty(messageName, messageName);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
+
+
+//    List<InlineKeyboardButton> buttons = Stream.of("run", "cancel")
+//            .map(buttonText -> {
+//                InlineKeyboardButton button = new InlineKeyboardButton(buttonText);
+//                button.setCallbackData("/" + buttonText);
+//                return button;
+//            })
+//            .collect(Collectors.toList());
+//
+//        answer.setReplyMarkup(new InlineKeyboardMarkup(
+//                new ArrayList<List<InlineKeyboardButton>>() {{
+//        add(buttons);
+//        }}
+//        ));
